a996a9a7546312c96ca850071556a7948f4286aa
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-12-30 11:31:26 +0400

    Language section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import './App.css'
-
+import { languages } from './Languages'
 function App() {
- 
+const languageElements = languages.map(language => {
+  const styles = {
+    backgroundColor:`${language.backgroundColor}`,
+              color:`${language.color}`
+  }
+  return (
+     <span 
+        className='chip'
+        style={styles}
+        key={language.name}
+     >
+        {language.name}
+    </span>
+  )
+}
+)
 
   return (
     <main>
-        <div className="title">
+        <header className="title">
           <h1>Assembly: End Game</h1>
           <p>Guess the word in 8 attempts to save the <code><strong>Programming</strong></code> world safe from Assembly!</p>
-        </div>
+        </header>
+        <section className="gameStatus">
+          <h2>Game Over!</h2>
+          <p>You lose!Better start learning Assembly😭</p>
+        </section>
+        <section className="languages">
+            {languageElements}
+        </section>
     </main>
   )
 }
