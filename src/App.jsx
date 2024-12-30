5d380dad209d34b611fd69d4112dbd0ecd78a6e5
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-12-30 10:40:57 +0400

    Assembly End Game

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <main>
+        <div className="title">
+          <h1>Assembly: End Game</h1>
+          <p>Guess the word in 8 attempts to save the <code><strong>Programming</strong></code> world safe from Assembly!</p>
+        </div>
+    </main>
+  )
+}
+
+export default App
